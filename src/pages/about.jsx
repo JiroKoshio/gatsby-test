34d8ed95c468e9e6be9a85db5c259d615b723e70
Jiro Koshio <koshio@web-shizuoka.net>
2020-07-04 21:27:26 +0900

    pushコマンドの確認2

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../layouts'
+
+export default () => (
+	  <Layout>
+	    <h1>My Wonderful Website About</h1>
+	    <p>This is the about page.</p>
+	  </Layout>
+)
